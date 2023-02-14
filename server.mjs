079145a12fb2a68ae0ad20679791add38cb0e289
Zachary Bedrosian <zachary@cleancult.com>
2023-02-14 10:59:20 -0500

    test

--- server.mjs
+++ server.mjs
@@ -3,7 +3,17 @@ import geoblaze from "geoblaze";
 import area from "@turf/area";
 import centroid from "@turf/centroid";
 
+const georaster = await geoblaze.parse(
+  `https://map-gules.vercel.app/maps/pop4.tif`
+);
+
+const getGeoValue = async () => {
+  const populationResult = await geoblaze.sum(georaster);
+  return populationResult;
+};
+
 createServer((req, res) => {
-  res.write("Hellsdfsdfo World!");
+  let value = getGeoValue();
+  res.write(value);
   res.end();
 }).listen(process.env.PORT);
